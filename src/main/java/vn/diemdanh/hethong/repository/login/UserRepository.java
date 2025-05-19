ee61633aeb79a2e88aa234f6454e70fef998208f
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-05-20 02:15:28 +0700

    Cấu hình JWT và đăng nhập với JWT

---
+++ src/main/java/vn/diemdanh/hethong/repository/login/UserRepository.java
@@ -0,0 +1,4 @@
+package vn.diemdanh.hethong.repository;
+
+public interface UserRepository extends org.springframework.data.jpa.repository.JpaRepository<vn.diemdanh.hethong.entity.User, java.lang.Long> {
+  }
---
+++ src/main/java/vn/diemdanh/hethong/security/WebSecurityConfig.java
@@ -0,0 +1,4 @@
+package vn.diemdanh.hethong.security;
+
+public class WebSecurityConfig {
+}
