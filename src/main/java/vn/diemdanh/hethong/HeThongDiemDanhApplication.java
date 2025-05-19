0197b02968ae76a982c3f24401fbf1b60d789625
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-05-19 23:36:53 +0700

    Initial commit

---
+++ src/main/java/vn/diemdanh/hethong/HeThongDiemDanhApplication.java
@@ -0,0 +1,13 @@
+package vn.diemdanh.hethong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeThongDiemDanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeThongDiemDanhApplication.class, args);
+	}
+
+}
