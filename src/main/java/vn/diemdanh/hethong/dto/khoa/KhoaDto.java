f4be2baef27e21f5ced7af2517965a1b71740f82
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-05-26 21:42:24 +0700

    Thêm API chức năng quản lý lớp và khoa

---
+++ src/main/java/vn/diemdanh/hethong/dto/khoa/KhoaDto.java
@@ -0,0 +1,9 @@
+package vn.diemdanh.hethong.dto.khoa;
+
+import lombok.Data;
+
+@Data
+public class KhoaDto {
+    private String maKhoa;
+    private String tenKhoa;
+}
---
+++ src/main/java/vn/diemdanh/hethong/dto/lop/LopDto.java
@@ -0,0 +1,13 @@
+package vn.diemdanh.hethong.dto.lop;
+
+import lombok.Data;
+
+@Data
+public class LopDto {
+    private String maLop;
+    private String tenLop;
+    private String maKhoa;
+    private String tenKhoa;
+    private String gvcn;
+    private String sdtGvcn;
+}
