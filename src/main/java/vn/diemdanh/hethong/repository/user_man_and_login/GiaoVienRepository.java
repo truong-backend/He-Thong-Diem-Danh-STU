7c9e9417b6c95309ffc104afb7e63409ee8ee669
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-06-04 21:35:09 +0700

    Danh sách giảng viên

--- src/main/java/vn/diemdanh/hethong/repository/user_man_and_login/GiaoVienRepository.java
+++ src/main/java/vn/diemdanh/hethong/repository/user_man_and_login/GiaoVienRepository.java
@@ -1,7 +1,10 @@
 package vn.diemdanh.hethong.repository.user_man_and_login;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.diemdanh.hethong.entity.GiaoVien;
 
 public interface GiaoVienRepository extends JpaRepository<GiaoVien, String> {
+    Page<GiaoVien> findByMaGvContainingIgnoreCase(String maGv, Pageable pageable);
 }
