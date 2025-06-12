0a5e799706ebe250add576d69c0586facbf6f144
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-06-13 04:45:08 +0700

    BACK UP lại dữ liệu trước

--- src/main/java/vn/diemdanh/hethong/dto/lichhoc/LichHocDto.java
+++ src/main/java/vn/diemdanh/hethong/dto/lichhoc/LichHocDto.java
@@ -6,17 +6,17 @@ import java.time.LocalDate;
 @Data
 public class LichHocDto {
     private String maSv;
-    private String tenSv;
+    private String tenSv= "";
     private Long maGd;
-    private String maGv;
-    private String tenGv;
-    private String maMh;
-    private String tenMh;
-    private Integer nmh;
-    private String phongHoc;
-    private LocalDate ngayBd;
-    private LocalDate ngayKt;
-    private Integer stBd;
-    private Integer stKt;
-    private Integer hocKy;
+    private String maGv= "";
+    private String tenGv= "";
+    private String maMh= "";
+    private String tenMh= "";
+    private Integer nmh= 0;
+    private String phongHoc= "";
+    private LocalDate ngayBd= null;
+    private LocalDate ngayKt= null;
+    private Integer stBd= 0;
+    private Integer stKt= 0;
+    private Integer hocKy= 0;
 } 
