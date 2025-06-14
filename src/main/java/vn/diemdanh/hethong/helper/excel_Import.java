a4289992949a541a0bbea32bdf5fb67eff8383b9
PGSTSHiepDuBai <nobitah2so4@gmail.com>
2025-06-14 15:35:09 +0700

    thêm import xlsx giáo viên

--- src/main/java/vn/diemdanh/hethong/helper/excel_Import.java
+++ src/main/java/vn/diemdanh/hethong/helper/excel_Import.java
@@ -1,10 +1,10 @@
 package vn.diemdanh.hethong.helper;
 
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import vn.diemdanh.hethong.dto.giaovien.GiaoVienDTOExcel;
+import vn.diemdanh.hethong.dto.giaovien.GiaoVienDto;
 import vn.diemdanh.hethong.dto.sinhvien.SinhVienExcelDto;
 
 import java.io.InputStream;
@@ -14,31 +14,77 @@ import java.util.Iterator;
 import java.util.List;
 
 public class excel_Import {
-    public static List<SinhVienExcelDto> excelImportFile(InputStream is){
+    public static List<SinhVienExcelDto> excelImportFile(InputStream is) {
         List<SinhVienExcelDto> list = new ArrayList<>();
         DataFormatter formatter = new DataFormatter();
 
-        try (Workbook wb = new XSSFWorkbook(is)){
-            Sheet sheet = wb.getSheetAt(0);
-            Iterator<Row> rows = sheet.iterator();
-            if(rows.hasNext()){
-                rows.next();
-            }
+        try (Workbook wb = new XSSFWorkbook(is)) {
+           for(int i = 0;i < wb.getNumberOfSheets();i++) {
+               Sheet sheet = wb.getSheetAt(0);
+               Iterator<Row> rows = sheet.iterator();
+               if (rows.hasNext()) {
+                   rows.next();
+               }
 
-            while(rows.hasNext()){
-                Row current = rows.next();
-                SinhVienExcelDto dto = new SinhVienExcelDto();
-                dto.setMaSv(formatter.formatCellValue(current.getCell(0)));
-                dto.setTenSv(formatter.formatCellValue(current.getCell(1)));
-                dto.setNgaySinh(LocalDate.parse(formatter.formatCellValue(current.getCell(2))));
-                dto.setPhai(Byte.parseByte(formatter.formatCellValue(current.getCell(3))));
-                dto.setDiaChi(formatter.formatCellValue(current.getCell(4)));
-                dto.setSdt(formatter.formatCellValue(current.getCell(5)));
-                dto.setEmail(formatter.formatCellValue(current.getCell(6)));
-                dto.setMaLop(formatter.formatCellValue(current.getCell(7)).trim());
-                dto.setAvatar(formatter.formatCellValue(current.getCell(8)));
-                list.add(dto);
-            }
+               while (rows.hasNext()) {
+                   Row current = rows.next();
+                   SinhVienExcelDto dto = new SinhVienExcelDto();
+                   dto.setMaSv(formatter.formatCellValue(current.getCell(0)));
+                   dto.setTenSv(formatter.formatCellValue(current.getCell(1)));
+                   dto.setNgaySinh(LocalDate.parse(formatter.formatCellValue(current.getCell(2))));
+                   dto.setPhai(Byte.parseByte(formatter.formatCellValue(current.getCell(3))));
+                   dto.setDiaChi(formatter.formatCellValue(current.getCell(4)));
+                   dto.setSdt(formatter.formatCellValue(current.getCell(5)));
+                   dto.setEmail(formatter.formatCellValue(current.getCell(6)));
+                   dto.setMaLop(formatter.formatCellValue(current.getCell(7)).trim());
+                   dto.setAvatar(formatter.formatCellValue(current.getCell(8)));
+                   list.add(dto);
+               }
+           }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
+    public static List<GiaoVienDTOExcel> importGiaoVienExcel(InputStream is) {
+        List<GiaoVienDTOExcel> list = new ArrayList<>();
+        DataFormatter formatter = new DataFormatter();
+        try (Workbook wb = new XSSFWorkbook(is)) {
+           for(int i = 0; i < wb.getNumberOfSheets();i++){
+               Sheet sheet = wb.getSheetAt(i);
+               Iterator<Row> rows = sheet.iterator();
+               if (rows.hasNext()) {
+                   rows.next();
+               }
+               while (rows.hasNext()) {
+                   Row current = rows.next();
+                   Cell cellMaGV = current.getCell(0);
+                   if(cellMaGV == null || formatter.formatCellValue(cellMaGV).isEmpty()){
+                       continue;
+                   }
+                   GiaoVienDTOExcel dto = new GiaoVienDTOExcel();
+                   dto.setMaGv(formatter.formatCellValue(current.getCell(0)));
+                   dto.setTenGv(formatter.formatCellValue(current.getCell(1)));
+
+                   String ngaySinhStr = formatter.formatCellValue(current.getCell(2));
+                   if(!ngaySinhStr.isEmpty()) {
+                       dto.setNgaySinh(LocalDate.parse(ngaySinhStr));
+                   } else {
+                       throw new RuntimeException("Ngày sinh bị trống tại dòng " + current.getRowNum());
+                   }
+                   String phaiStr = formatter.formatCellValue(current.getCell(3));
+                   if(!phaiStr.isEmpty()) {
+                       dto.setPhai(Byte.parseByte(phaiStr));
+                   } else {
+                       throw new RuntimeException("Giới tính bị trống tại dòng " + current.getRowNum());
+                   }                   dto.setDiaChi(formatter.formatCellValue(current.getCell(4)));
+                   dto.setSdt(formatter.formatCellValue(current.getCell(5)));
+                   dto.setEmail(formatter.formatCellValue(current.getCell(6)));
+                   dto.setAvatar(formatter.formatCellValue(current.getCell(7)));
+                   list.add(dto);
+               }
+           }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
