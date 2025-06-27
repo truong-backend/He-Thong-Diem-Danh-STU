9fc5b86e27479b901c7d599e704615dca9a9c22f
PGSTSHiepDuBai <nobitah2so4@gmail.com>
2025-06-27 17:06:17 +0700

    fixed theo nhánh BACK-END

--- src/main/java/vn/diemdanh/hethong/repository/LichGdRepository.java
+++ src/main/java/vn/diemdanh/hethong/repository/LichGdRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import vn.diemdanh.hethong.entity.GiaoVien;
 import vn.diemdanh.hethong.entity.LichGd;
 import vn.diemdanh.hethong.entity.MonHoc;
@@ -17,18 +18,19 @@ public interface LichGdRepository extends JpaRepository<LichGd, Long> {
 
     // 1. LẤY DANH SÁCH HỌC KỲ THEO NĂM HỌC
     @Query(value = """
-        SELECT DISTINCT 
-            hoc_ky,
-            CASE 
-                WHEN hoc_ky = 1 THEN CONCAT('HK1 - Năm học ', (YEAR(ngay_bd) - 1), ' - ', YEAR(ngay_bd))
-                WHEN hoc_ky = 2 THEN CONCAT('HK2 - Năm học ', (YEAR(ngay_bd) - 1), ' - ', YEAR(ngay_bd))
-                WHEN hoc_ky = 3 THEN CONCAT('HK3 - Năm học ', YEAR(ngay_bd), ' - ', YEAR(ngay_bd))
-                ELSE CONCAT('HK', hoc_ky, ' - Năm học ', YEAR(ngay_bd))
-            END as hoc_ky_display,
-            YEAR(ngay_bd) as nam_hoc
-        FROM lich_gd 
-        ORDER BY nam_hoc DESC, hoc_ky
-        """, nativeQuery = true)
-    List<Object[]> findAllSemesters();
+    SELECT DISTINCT 
+        hoc_ky,
+        CASE 
+            WHEN hoc_ky = 1 THEN CONCAT('HK1 - Năm học ', (YEAR(ngay_bd) - 1), ' - ', YEAR(ngay_bd))
+            WHEN hoc_ky = 2 THEN CONCAT('HK2 - Năm học ', (YEAR(ngay_bd) - 1), ' - ', YEAR(ngay_bd))
+            WHEN hoc_ky = 3 THEN CONCAT('HK3 - Năm học ', YEAR(ngay_bd), ' - ', YEAR(ngay_bd))
+            ELSE CONCAT('HK', hoc_ky, ' - Năm học ', YEAR(ngay_bd))
+        END as hoc_ky_display,
+        YEAR(ngay_bd) as nam_hoc
+    FROM lich_gd 
+    WHERE ma_gv = :maGv
+    ORDER BY nam_hoc DESC, hoc_ky
+    """, nativeQuery = true)
+    List<Object[]> findAllSemesters(@Param("maGv") String maGv);
 
 } 
