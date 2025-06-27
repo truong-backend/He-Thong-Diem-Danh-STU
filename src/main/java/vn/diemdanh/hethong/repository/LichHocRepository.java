39085f51be00872266bc426e1b645b26a2b028fe
PGSTSHiepDuBai <nobitah2so4@gmail.com>
2025-06-27 17:07:10 +0700

    repo tìm sinh viên theo mã tkb

--- src/main/java/vn/diemdanh/hethong/repository/LichHocRepository.java
+++ src/main/java/vn/diemdanh/hethong/repository/LichHocRepository.java
@@ -3,11 +3,14 @@ package vn.diemdanh.hethong.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import vn.diemdanh.hethong.entity.LichHoc;
 import vn.diemdanh.hethong.entity.LichHocId;
 import vn.diemdanh.hethong.entity.SinhVien;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface LichHocRepository extends JpaRepository<LichHoc, LichHocId> {
     Page<LichHoc> findByMaSv(SinhVien sinhVien, Pageable pageable);
@@ -20,4 +23,13 @@ public interface LichHocRepository extends JpaRepository<LichHoc, LichHocId> {
             LocalDate endDate2,
             Pageable pageable
     );
+    @Query(value = """
+        select count(*)
+            from sinh_vien sv
+            join lich_hoc lh on sv.ma_sv = lh.ma_sv
+            join lich_gd gd on gd.ma_gd = lh.ma_gd
+            join tkb t on t.ma_gd = gd.ma_gd
+        where sv.ma_sv = :maSv and t.ma_tkb = :maTkb
+    """,nativeQuery = true)
+    Integer findSinhVienByMaTkb(@Param("maSv")String maSv,@Param("maTkb")int maTkb);
 } 
