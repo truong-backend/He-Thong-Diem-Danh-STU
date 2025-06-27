68058d2cf3a0b9f0c0b4ccbde0c68bba9b81062d
PGSTSHiepDuBai <nobitah2so4@gmail.com>
2025-06-27 17:05:35 +0700

    thêm repo lấy mã Giáo viên

--- src/main/java/vn/diemdanh/hethong/repository/GiaoVienRepository.java
+++ src/main/java/vn/diemdanh/hethong/repository/GiaoVienRepository.java
@@ -3,8 +3,13 @@ package vn.diemdanh.hethong.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import vn.diemdanh.hethong.entity.GiaoVien;
 
 public interface GiaoVienRepository extends JpaRepository<GiaoVien, String> {
     Page<GiaoVien> findByMaGvContainingIgnoreCase(String maGv, Pageable pageable);
+
+    @Query("SELECT gv.maGv FROM GiaoVien gv WHERE gv.email = :email")
+    String findMaGvByEmail(@Param("email") String email);
 }
