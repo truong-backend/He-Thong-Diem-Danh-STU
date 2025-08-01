e685fea6d7a81703b56f248354aaf4ac69ed1ed4
Tran Ngoc Hiep <nobitah2so4@gmail.com>
2025-08-01 23:25:05 +0700

    Thống Kê điểm danh dto

---
+++ src/main/java/vn/diemdanh/hethong/dto/diemdanh/ThongKeDiemDanhDTO.java
@@ -0,0 +1,17 @@
+package vn.diemdanh.hethong.dto.diemdanh;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ThongKeDiemDanhDTO {
+    private String maSv;
+    private String tenSv;
+    private String tenLop;
+    private Long so_buoi_hoc;
+    private Long so_buoi_diem_danh;
+    private Long so_buoi_chua_diem_danh;
+
+}
