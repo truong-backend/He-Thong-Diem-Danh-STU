32e4c1826bf640a75ed32e629edbf98d6f5d6d5c
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2025-08-04 16:27:43 +0700

    Team FIX 5

--- src/main/java/vn/diemdanh/hethong/config/AwsConfig.java
+++ src/main/java/vn/diemdanh/hethong/config/AwsConfig.java
@@ -6,28 +6,23 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsConfig {
-    private BasicAWSCredentials awsCreds() {
-        return new BasicAWSCredentials(
-                System.getenv("AWS_ACCESSKEY"),
-                System.getenv("AWS_SECRETKEY")
-        );
-    }
     @Bean
     public AmazonRekognition rekognitionClient() {
         return AmazonRekognitionClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds()))
-                .withRegion(Regions.AP_SOUTHEAST_1)
-                .build();
+                .withRegion(Regions.AP_SOUTHEAST_1) // vùng phù hợp
+                .build(); // tự động lấy credentials từ ~/.aws/credentials
     }
+
     @Bean
-    public AmazonS3Client amazonS3(){
-        return (AmazonS3Client) AmazonS3Client.builder()
+    public AmazonS3Client amazonS3() {
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.AP_SOUTHEAST_1)
-                .build();
+                .build(); // tự động lấy credentials
     }
 }
